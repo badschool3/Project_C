5bfe9de26f4cfdedbdb06e73c3b568e33b3afde6
badschool3 <godmaste5@gmail.com>
2018-11-12 21:21:15 +0900

    base.h에 compare함수 추가

--- 2/base.h
+++ 2/base.h
@@ -28,3 +28,16 @@ typedef struct member{
 	char phoneNum[12];
 
 }M;
+
+int compare(const void *a, const void *b)
+{
+	int num1 = *(int *)a;
+	int num2 = *(int *)b;
+	if (num1 < num2)
+		return -1;
+	else if (num2 > num1)
+		return 1;
+	else
+		return 0;
+}
+
